6d32444d673efeaf90ede737525da314729cbd39
Zainab Ahmad <zainabalazzawi91@gmail.com>
2025-04-11 14:59:36 +0300

    fix build

--- app/api/projects/[id]/route.ts
+++ app/api/projects/[id]/route.ts
@@ -5,8 +5,7 @@ import { authOptions } from "../../auth/lib/auth";
 
 export async function GET(
   request: Request,
-  { params }: { params: { id: string } }
-) {
+  { params }: { params: Promise<{ id: string }> }) {
   try {
     const session = await getServerSession(authOptions);
 
