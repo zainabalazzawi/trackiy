0d59fd76bb6d117f4bdda076d7743529ec9dee7a
Zainab Ahmad <zainabalazzawi91@gmail.com>
2025-10-22 19:39:01 +0300

    typingUser

--- app/projects/[id]/tickets/[ticketId]/page.tsx
+++ app/projects/[id]/tickets/[ticketId]/page.tsx
@@ -69,14 +69,6 @@ const TicketPage = () => {
 
   return (
     <div className="p-6 w-full">
-
-
-
-
-
-
-
-
       {ticket.ticketNumber}
       <div className="flex gap-8">
         <div className="w-[70%]">
@@ -85,7 +77,7 @@ const TicketPage = () => {
               value={ticket.title}
               onSave={(value) => updateTicket({ title: value })}
               titleText
-
+              fieldId="title"
             />
 
             <div className="flex items-center gap-60 mt-3">
@@ -104,7 +96,7 @@ const TicketPage = () => {
             onSave={(value) => updateTicket({ description: value })}
             label="Description"
             type="textarea"
-
+            fieldId="description"
           />
 
           {/* Comments Section */}
